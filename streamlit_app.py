69c4ec3954c714ba10a1ec00057e7fea6884b60a
varunmayaluru <varun.mjrph@gmail.com>
2024-09-22 23:45:19 +0530

    Update labels in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,40 +11,45 @@ scaler = joblib.load('app/artifacts/scaler.pkl')
 
 # Function to get user inputs from the Streamlit interface
 def get_user_input():
-    gender = st.selectbox('Gender', [0, 1])
+    gender = st.selectbox('Gender', ['Male', 'Female'])
     age = st.slider('Age', 20, 100, 60)
-    smoking = st.selectbox('Smoking', [0, 1])
-    yellow_fingers = st.selectbox('Yellow Fingers', [0, 1])
-    anxiety = st.selectbox('Anxiety', [0, 1])
-    peer_pressure = st.selectbox('Peer Pressure', [0, 1])
-    chronic_disease = st.selectbox('Chronic Disease', [0, 1])
-    fatigue = st.selectbox('Fatigue', [0, 1])
-    allergy = st.selectbox('Allergy', [0, 1])
-    wheezing = st.selectbox('Wheezing', [0, 1])
-    alcohol_consuming = st.selectbox('Alcohol Consuming', [0, 1])
-    coughing = st.selectbox('Coughing', [0, 1])
-    shortness_of_breath = st.selectbox('Shortness of Breath', [0, 1])
-    swallowing_difficulty = st.selectbox('Swallowing Difficulty', [0, 1])
-    chest_pain = st.selectbox('Chest Pain', [0, 1])
+    smoking = st.selectbox('Smoking', ['Yes', 'No'])
+    yellow_fingers = st.selectbox('Yellow Fingers', ['Yes', 'No'])
+    anxiety = st.selectbox('Anxiety', ['Yes', 'No'])
+    peer_pressure = st.selectbox('Peer Pressure', ['Yes', 'No'])
+    chronic_disease = st.selectbox('Chronic Disease', ['Yes', 'No'])
+    fatigue = st.selectbox('Fatigue', ['Yes', 'No'])
+    allergy = st.selectbox('Allergy', ['Yes', 'No'])
+    wheezing = st.selectbox('Wheezing', ['Yes', 'No'])
+    alcohol_consuming = st.selectbox('Alcohol Consuming', ['Yes', 'No'])
+    coughing = st.selectbox('Coughing', ['Yes', 'No'])
+    shortness_of_breath = st.selectbox('Shortness of Breath', ['Yes', 'No'])
+    swallowing_difficulty = st.selectbox('Swallowing Difficulty', ['Yes', 'No'])
+    chest_pain = st.selectbox('Chest Pain', ['Yes', 'No'])
 
-    # Create a dictionary with all the inputs
+    # Map user-friendly input back to the original values (0 or 1)
+    gender_mapping = {'Male': 1, 'Female': 0}
+    yes_no_mapping = {'Yes': 1, 'No': 0}
+
+    # Create a dictionary with all the inputs mapped to numerical values
     user_data = {
-        'gender' : gender,
+        'gender': gender_mapping[gender],
         'age': age,
-        'smoking': smoking,
-        'yellow_fingers': yellow_fingers,
-        'anxiety': anxiety,
-        'peer_pressure': peer_pressure,
-        'chronic_disease': chronic_disease,
-        'fatigue': fatigue,
-        'allergy': allergy,
-        'wheezing': wheezing,
-        'alcohol_consuming': alcohol_consuming,
-        'coughing': coughing,
-        'shortness_of_breath': shortness_of_breath,
-        'swallowing_difficulty': swallowing_difficulty,
-        'chest_pain': chest_pain
+        'smoking': yes_no_mapping[smoking],
+        'yellow_fingers': yes_no_mapping[yellow_fingers],
+        'anxiety': yes_no_mapping[anxiety],
+        'peer_pressure': yes_no_mapping[peer_pressure],
+        'chronic_disease': yes_no_mapping[chronic_disease],
+        'fatigue': yes_no_mapping[fatigue],
+        'allergy': yes_no_mapping[allergy],
+        'wheezing': yes_no_mapping[wheezing],
+        'alcohol_consuming': yes_no_mapping[alcohol_consuming],
+        'coughing': yes_no_mapping[coughing],
+        'shortness_of_breath': yes_no_mapping[shortness_of_breath],
+        'swallowing_difficulty': yes_no_mapping[swallowing_difficulty],
+        'chest_pain': yes_no_mapping[chest_pain]
     }
+
     return user_data
 
 # Main Streamlit app
